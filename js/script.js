ae3428d8de74ec9eb881da49a9bc67aae271ad91
Irina Andre <vir_andre@mail.ru>
2021-11-21 16:47:01 +0200

    HW_3

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//task #1
+let admin;
+
+let name;
+
+name = 'Джон';
+
+admin = name;
+
+alert(admin);
+
+//task #2
+let ourPlanetName;
+
+let currentUserName;
+
+//task #3
+//1010 string, если у одного из операндов тип данных string, то оператор + склеивает их значения в одну строку
+let result = '10' + 10;
+console.log(result, typeof result);
